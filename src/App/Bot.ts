26f4f608264b6460a61876380c637d1ddc59a16a
Adhil <adhil.mhdk28@gmail.com>
2023-03-03 20:58:16 +0530

    version: 2.0

--- src/App/Bot.ts
+++ src/App/Bot.ts
@@ -2,21 +2,61 @@ import { Bot } from "grammy"
 import env from "./env";
 import OpenAi from "./OpenAi";
 import database from "./Database"
+import utils from "./Utils"
+import { createWriteStream, readFileSync, unlinkSync } from "fs"
+import axios from "axios";
 
 const bot = new Bot(env.BotToken());
 
 bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
-bot.command("clear",(ctx)=>{
+bot.command("clear", (ctx) => {
     database.clear(ctx.chat.id)
     ctx.reply('chat cleared')
+})
+bot.on('message:photo', async (ctx) => {
+    console.log('photo msg');
+
+    const loadingProcess = await loadingMessage(ctx.chat.id)
+
+
+    var file = await ctx.getFile()
+    const downloadUrl = `https://api.telegram.org/file/bot${env.BotToken()}/${file.file_path}`
+    const r = await axios({
+        url: downloadUrl,
+        method: 'GET',
+        responseType: 'stream'
+    });
+    r.data.pipe(createWriteStream(`./${ctx.chat.id}.png`)).on('finish', async () => {
+        let q = await utils.extract(readFileSync(`./${ctx.chat.id}.png`)) as string
+
+
+        const prompt = `${database.read(ctx.chat.id)}\n\nUser:${q}\nBot:`
+        console.log(ctx.chat.id, " : ", q);
+        const response = await OpenAi.generate(prompt)
+        const message = loadingProcess.message()
+        if (response == '') {
+            return
+        }
+        if (pretty(response) != '') {
+            await bot.api.editMessageText(ctx.chat.id, message.message_id, pretty(response))
+        } else {
+            await bot.api.editMessageText(ctx.chat.id, message.message_id, response)
+        }
+        unlinkSync(`./${ctx.chat.id}.png`)
+        database.save({ chatId: ctx.chat.id, text: `User: ${q}\nBot: ${pretty(response)}` })
+    })
+
+
+
+
 })
 bot.on('message:text', async (ctx) => {
     const loadingProcess = await loadingMessage(ctx.chat.id)
     const prompt = `${database.read(ctx.chat.id)}\n\nUser:${ctx.message.text}\nBot:`
-    console.log(ctx.chat.id," : ",ctx.message.text);
+    console.log(ctx.chat.id, " : ", ctx.message.text);
     const response = await OpenAi.generate(prompt)
     const message = loadingProcess.message()
-    if (response=='') {
+    if (response == '') {
         return
     }
     if (pretty(response) != '') {
---
+++ src/App/Utils.ts
@@ -0,0 +1,26 @@
+import { Tesseract } from "tesseract.ts";
+
+
+function extract(image: any) {
+    return new Promise((r) => {
+        Tesseract
+            .recognize(image)
+            .then((res: any) => {
+                let text = filter(res.text)
+                r(text)
+            })
+            .catch(() => { r('') });
+    })
+}
+function filter(text: string) {
+    let scrs = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "-", "=", ",", ".", "/", "?", "<", ">", ";", ":", "'", '"', "\\", "|", "[", "]", "{", "}", "`", "~"]
+    let filterText = text.split('\n').filter((v)=>{
+        if (!scrs.includes(v[0]) && v[0]!='') {
+            return v
+        }else{
+            return null
+        }
+    })
+    return filterText.join(' ')
+}
+export default { extract }
