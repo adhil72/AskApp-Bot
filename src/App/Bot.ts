f95b7dc971598c70755f2982b9f1e02fd6901a35
adhil72 <126852465+adhil72@users.noreply.github.com>
2023-03-04 07:33:11 +0530

    Update Bot.ts

--- src/App/Bot.ts
+++ src/App/Bot.ts
@@ -18,7 +18,7 @@ bot.command("image", async (ctx) => {
     let prompt = ctx.message.text.replace("/image","")
 console.log(prompt)
     const response = await OpenAi.image(prompt)
-    ctx.reply(response)
+    ctx.replyWithPhoto(response)
 
 })
 bot.on('message:photo', async (ctx) => {
